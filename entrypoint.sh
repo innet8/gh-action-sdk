4a31149361b093aac90ef7a1d10a9fadbd3d7984
zzzzzhy <azure.and.sea@gmail.com>
2022-09-18 02:23:14 +0800

    Update entrypoint.sh
    Signed-off-by: zzzzzhy <azure.and.sea@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,75 +51,79 @@ if [ -z "$PACKAGES" ]; then
 		V="$V" \
 		-j "$(nproc)"
 else
-	# compile specific packages with checks
-	for PKG in $PACKAGES; do
-		for FEED in $ALL_CUSTOM_FEEDS; do
-			./scripts/feeds install -p "$FEED" -f "$PKG"
-		done
-		make \
-			BUILD_LOG="$BUILD_LOG" \
-			IGNORE_ERRORS="$IGNORE_ERRORS" \
-			"package/$PKG/download" V=s || \
-				exit $?
-
-		make \
-			BUILD_LOG="$BUILD_LOG" \
-			IGNORE_ERRORS="$IGNORE_ERRORS" \
-			"package/$PKG/check" V=s 2>&1 | \
-				tee logtmp
-
-		RET=${PIPESTATUS[0]}
-
-		if [ "$RET" -ne 0 ]; then
-			echo_red   "=> Package check failed: $RET)"
-			exit "$RET"
-		fi
-
-		badhash_msg="HASH does not match "
-		badhash_msg+="|HASH uses deprecated hash,"
-		badhash_msg+="|HASH is missing,"
-		if grep -qE "$badhash_msg" logtmp; then
-			echo "Package HASH check failed"
-			exit 1
-		fi
-
-		PATCHES_DIR=$(find /feed -path "*/$PKG/patches")
-		if [ -d "$PATCHES_DIR" ] && [ -z "$NO_REFRESH_CHECK" ]; then
+	if [ -z "$CHECKS" ]; then
+		# compile specific packages with checks
+		for PKG in $PACKAGES; do
+			for FEED in $ALL_CUSTOM_FEEDS; do
+				./scripts/feeds install -p "$FEED" -f "$PKG"
+			done
 			make \
 				BUILD_LOG="$BUILD_LOG" \
 				IGNORE_ERRORS="$IGNORE_ERRORS" \
-				"package/$PKG/refresh" V=s || \
+				"package/$PKG/download" V=s || \
 					exit $?
 
-			if ! git -C "$PATCHES_DIR" diff --quiet -- .; then
-				echo "Dirty patches detected, please refresh and review the diff"
-				git -C "$PATCHES_DIR" checkout -- .
+			make \
+				BUILD_LOG="$BUILD_LOG" \
+				IGNORE_ERRORS="$IGNORE_ERRORS" \
+				"package/$PKG/check" V=s 2>&1 | \
+					tee logtmp
+
+			RET=${PIPESTATUS[0]}
+
+			if [ "$RET" -ne 0 ]; then
+				echo_red   "=> Package check failed: $RET)"
+				exit "$RET"
+			fi
+
+			badhash_msg="HASH does not match "
+			badhash_msg+="|HASH uses deprecated hash,"
+			badhash_msg+="|HASH is missing,"
+			if grep -qE "$badhash_msg" logtmp; then
+				echo "Package HASH check failed"
 				exit 1
 			fi
-		fi
 
-		FILES_DIR=$(find /feed -path "*/$PKG/files")
-		if [ -d "$FILES_DIR" ] && [ -z "$NO_SHFMT_CHECK" ]; then
-			find "$FILES_DIR" -name "*.init" -exec shfmt -w -sr -s '{}' \;
-			if ! git -C "$FILES_DIR" diff --quiet -- .; then
-				echo "init script must be formatted. Please run through shfmt -w -sr -s"
-				git -C "$FILES_DIR" checkout -- .
-				exit 1
+			PATCHES_DIR=$(find /feed -path "*/$PKG/patches")
+			if [ -d "$PATCHES_DIR" ] && [ -z "$NO_REFRESH_CHECK" ]; then
+				make \
+					BUILD_LOG="$BUILD_LOG" \
+					IGNORE_ERRORS="$IGNORE_ERRORS" \
+					"package/$PKG/refresh" V=s || \
+						exit $?
+
+				if ! git -C "$PATCHES_DIR" diff --quiet -- .; then
+					echo "Dirty patches detected, please refresh and review the diff"
+					git -C "$PATCHES_DIR" checkout -- .
+					exit 1
+				fi
 			fi
-		fi
 
-	done
+			FILES_DIR=$(find /feed -path "*/$PKG/files")
+			if [ -d "$FILES_DIR" ] && [ -z "$NO_SHFMT_CHECK" ]; then
+				find "$FILES_DIR" -name "*.init" -exec shfmt -w -sr -s '{}' \;
+				if ! git -C "$FILES_DIR" diff --quiet -- .; then
+					echo "init script must be formatted. Please run through shfmt -w -sr -s"
+					git -C "$FILES_DIR" checkout -- .
+					exit 1
+				fi
+			fi
 
-	make \
-		-f .config \
-		-f tmp/.packagedeps \
-		-f <(echo "\$(info \$(sort \$(package-y) \$(package-m)))"; echo -en "a:\n\t@:") \
-			| tr ' ' '\n' > enabled-package-subdirs.txt
+		done
 
+		make \
+			-f .config \
+			-f tmp/.packagedeps \
+			-f <(echo "\$(info \$(sort \$(package-y) \$(package-m)))"; echo -en "a:\n\t@:") \
+				| tr ' ' '\n' > enabled-package-subdirs.txt
+	fi
+	
 	for PKG in $PACKAGES; do
-		if ! grep -m1 -qE "(^|/)$PKG$" enabled-package-subdirs.txt; then
-			echo "::warning file=$PKG::Skipping $PKG due to unsupported architecture"
-			continue
+		if [ -z "$CHECKS" ]; then
+			if ! grep -m1 -qE "(^|/)$PKG$" enabled-package-subdirs.txt; then
+				echo "::warning file=$PKG::Skipping $PKG due to unsupported architecture"
+				continue
+			fi
 		fi
 
 		make \
