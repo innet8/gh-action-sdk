ee9dfe45d16fde939432f614475cc9c457458ead
zzzzzhy <azure.and.sea@gmail.com>
2022-09-22 00:58:22 +0800

    Update glinet_sdk.sh
    Signed-off-by: zzzzzhy <azure.and.sea@gmail.com>

--- glinet_sdk.sh
+++ glinet_sdk.sh
@@ -44,8 +44,8 @@ download_sdk() {
     printf "\nUse 'builder.sh script to compile all your packages.\nRun './builder.sh' to get more help.\n\n"
     if [ $version == 'qsdk11' ]; then
     	local n=$(grep -n '$(Build/Patch)' include/package-defaults.mk |awk -F':' '{print $1}')
-	sed -i ''${n}' i [ ! -d ./src/ ] || $(CP) ./src/. $(PKG_BUILD_DIR)' include/package-defaults.mk
-    fi
+	sed -i ''${n}' i \\t[ ! -d ./src/ ] || $(CP) ./src/. $(PKG_BUILD_DIR)' include/package-defaults.mk
+    fi	
 }
 
 compile_sdk(){
